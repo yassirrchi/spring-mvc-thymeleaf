d4b2c13cb3828006f1a88590ee93b21b48c4ec96
yassirrchi <yassirrchi7@gmail.com>
2022-04-20 21:25:11 +0000

    edit + add

--- src/main/java/com/patientmvc/patientmvc/PatientMvcApplication.java
+++ src/main/java/com/patientmvc/patientmvc/PatientMvcApplication.java
@@ -20,7 +20,7 @@ public class PatientMvcApplication {
         return  args ->{
          //   patientRepository.save(new Patient(null,"hamid",new Date(),false,12));
            // patientRepository.save(new Patient(null,"hasbulla",new Date(),true,142));
-            patientRepository.save(new Patient(null,"yassir",new Date(),false,12));
+           // patientRepository.save(new Patient(null,"yassir",new Date(),false,12));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
--- src/main/java/com/patientmvc/patientmvc/entities/Patient.java
+++ src/main/java/com/patientmvc/patientmvc/entities/Patient.java
@@ -2,20 +2,28 @@ package com.patientmvc.patientmvc.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.*;
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min=4,max=40)
     private String nom;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 
 }
